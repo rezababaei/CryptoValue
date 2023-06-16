d77964a9fdd8aee6c9931282f19b127cbf03bcfc
Reza Babaei <rzbabaei1@gmail.com>
2023-06-16 12:59:07 +0200

    WatchListFragment added Setting up a Navigation Bottom

--- app/src/main/java/com/example/cryptovalue/MainActivity.java
+++ app/src/main/java/com/example/cryptovalue/MainActivity.java
@@ -2,19 +2,33 @@ package com.example.cryptovalue;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
-import android.database.DatabaseUtils;
 import android.os.Bundle;
 
 import com.example.cryptovalue.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    NavHostFragment navHostFragment;
+    NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        setupNavigationComponent();
+    }
+
+    private void setupNavigationComponent() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
